67113dcd89a13651d2eb32267ffc9b1056092559
Georges Monette <georges@yorku.ca>
2016-04-07 21:39:38 -0400

    update contents

---
+++ contents.R
@@ -0,0 +1,81 @@
+#' ---
+#' title: "Topics in Statistics"
+#' author: 
+#' - name: Georges Monette
+#'   affiliation: York University
+#' date: "`r format(Sys.time(), '%B %d, %Y at %H:%M')`"
+#' output:
+#'    html_document:
+#'      toc: true
+#'      toc_depth: 6
+#' ---
+#' <!--
+#' TODO:
+#' - add graphics, programming, functions and debugging 
+#' from  C:\home\Courses\R\Scripts\Day_3 
+#' - integrate
+#' -->
+#' 
+#' ## Part 1: Introduction
+#' 
+#' **Slides:** [Lies, Damned Lies and Statistics](Lies_Damned_Lies_And_Statistics.pdf)
+#' 
+#' **Summary:** Many textbooks warn that it is important to be
+#' critical when assessing claims based on analyses of data.
+#' Yet few say how. The theme of this presentation is
+#' most cases of statistical confusion arise from failing to
+#' make two important distinctions.  There are two types of 
+#' questions: causal questions and 'predictive' questions, and
+#' there are two types of data: observational data -- in which 
+#' levels of variables are not manipulated -- and 
+#' experimental data in which levels of variables are assigned
+#' under the control of an experimenter.  
+#' 
+#' 'Spurious correlation' is not at all spurious. It is merely
+#' 'misunderstood correlation'.  Most perceptions that statistics
+#' lie come from failing to make the distinction between
+#' association and causation and failing to understand
+#' that the rationale for causation comes not so much from 
+#' the data themselves but from the way in which the data
+#' were collected, specifically the way in which the levels of 
+#' variables were assigned.
+#' 
+#' **Wiki discussion:** 
+#' 
+#' **Exercises:**
+#' 
+#' 1. Find links to news or blog articles that discuss a current
+#'    or recent controversial -- or, at least, debatable -- 
+#'    effect of intervention (a drug, diet, health practice,
+#'    social or economic policy, etc.) on some outcome (risk of
+#'    disease, social outcome, etc.).  Discuss the nature of the
+#'    available evidence.  Is it observational or experimental? 
+#'    Are the relevant questions causal or predictive?  How does
+#'    your analysis help to clarify the controversy?
+#'    
+#' 2. Find a link to a major journal article in a field in 
+#'    which you are interested. Are the conclusions based on
+#'    evidence and analysis of data? Are the major questions 
+#'    causal or predictive? What kind of data were used?  
+#'    Is the structure of the data appropriate for the intended
+#'    questions? If causal conclusions are based on observational
+#'    data, what strategies did the article use to justify
+#'    causal conclusions?
+#' 
+#' ## Part 2: Installing R and key packages
+#' 
+#' 
+#' ## Appendix 1: Future title
+#' 
+#' ## Appendix 2: Material in Progress
+#' 
+#' 1. [Quick useful R recipes](http://capstone.stats.yorku.ca/index.php/R_Recipes)  (on our wiki so it's easy to add to them)
+#' 
+#' Add
+#' 1. Reading and merging spreadsheets ... .R
+#' <!--- TO ADD:
+#' Comments on Assignment 2.R
+#' --->
+#'     
+#'                 
+
